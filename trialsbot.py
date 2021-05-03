20b700ed1a6ba7d45a5e765852da1f40533d8775
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-05-02 23:22:32 -0300

    Instalada verificação de ChannelId

--- trialsbot.py
+++ trialsbot.py
@@ -1,5 +1,6 @@
 import discord
 import pyrez
+import asyncio
 
 #Código para resgatar o token do Bot
 def read_token():
@@ -25,8 +26,11 @@ client = discord.Client()
 
 #Código quando é enviado uma mensagem ao Bot
 @client.event
+
+#Comandos
 async def on_message(message):
     if message.content == ".help":
+
         embed = discord.Embed(title="Central de Ajuda do TrialsBot", description='Alguns comandos para facilitar a moderação \n Lembrando que todos os comando devem ser seguidos por pelo jogo com o "-jogo", por exemplo: \n'
                                                                                  '.stats-paladins ou .stats-smite')
         embed.add_field(name=".stats", value="Retorna o arquivo de texto com todos os stats da partida")
@@ -37,47 +41,59 @@ async def on_message(message):
     if message.content.find(".stats") != -1: #Retorna stats.txt no discord
         await message.channel.send(content="Comando recebido", embed=None)
         mensagem = message.content
-        retorno = ReconheceJogo(mensagem)
+        channelId = message.channel.id
+        retorno = Refatorando(channelId, mensagem)
         game = retorno[0]
-        comando = retorno[1]
-        if game == "Error": await message.channel.send(content="Comando incorreto, o comando sempre deve ser acompanhado pelo jogo, detalhes em .help")
-        match_id = ReconhecerComando(mensagem, comando)
-        print(match_id)
-        print(game)
+        match_id = retorno[1]
         PegarStats(match_id, game)
         with open("teste.txt", 'rb') as file:
             await message.channel.send(file=discord.File(file, "Stats.txt"))
 
     if message.content.find(".id") != -1: #Retorna resultado da partida e ID dos players
-        print('Comando recebido')
         await message.channel.send(content="Comando recebido", embed=None)
         mensagem = message.content
-        retorno = ReconheceJogo(mensagem)
+        channelId = message.channel.id
+        retorno = Refatorando(channelId, mensagem)
         game = retorno[0]
-        comando = retorno[1]
-        if game == "Error": await message.channel.send(content="Comando incorreto, o comando sempre deve ser acompanhado pelo jogo, detalhes em .help")
-        match_id = ReconhecerComando(mensagem, comando)
+        match_id = retorno[1]
         details_players = PegarId(match_id, game)
-        await message.channel.send(content=details_players)
+        Envia_Msg(channelId, details_players)
 
     if message.content.find(".replay") != -1: #Retorna se possui replay ou não
         await message.channel.send(content="Comando recebido", embed=None)
         mensagem = message.content
-        retorno = ReconheceJogo(mensagem)
+        channelId = message.channel.id
+        retorno = Refatorando(channelId, mensagem)
         game = retorno[0]
-        comando = retorno[1]
-        if game == "Error": await message.channel.send(content="Comando incorreto, o comando sempre deve ser acompanhado pelo jogo, detalhes em .help")
-        match_id = ReconhecerComando(mensagem, comando)
+        match_id = retorno[1]
         details_players = PegarReplay(match_id, game)
-        await message.channel.send(content=details_players)
+        Envia_Msg(channelId, details_players)
+
+    if message.content.find(".teste") != -1:
+        print("comando recebido")
+        id = message.channel.id
+        response = ConfereDiscId(id)
+        if not response: raise ValueError("Server fora da Whitelist")
+        print(response)
+
 #Funções
+
+def Refatorando(channelId, mensagem ):
+    response = ConfereDiscId(channelId)
+    RaiseChannelError(channelId, response)
+    retorno = ReconheceJogo(channelId, mensagem)
+    game = retorno[0]
+    comando = retorno[1]
+    match_id = ReconhecerComando(mensagem, comando)
+    retorno = [game, match_id]
+    return retorno
 
 def ReconhecerComando(mensagem, content):
     input_comando = mensagem
     output_comando = input_comando.replace(content, "")
     return output_comando
 
-def ReconheceJogo(mensagem):
+def ReconheceJogo (channel_id, mensagem):
     comandos = [".id", ".stats", ".replay"]
     jogos = {"smite": smite_req, "paladins": paladins_req}
     for jogo in jogos:
@@ -87,12 +103,18 @@ def ReconheceJogo(mensagem):
             break
         else:
             game = "Error"
+            str_jogo = ""
     for command in comandos:
         if mensagem.find(command) != -1:
             str_comando = command
             comando = str_comando + "-" + str_jogo + " "
             break
-    retorno = [game,comando]
+    retorno = [game, comando]
+    mensagem = "Comando incorreto, o comando sempre deve ser acompanhado pelo jogo, detalhes em .help"
+    if game == "Error":
+        Envia_Msg(channel_id, mensagem)
+        raise LookupError("Comando não acompanhado pelo jogo")
+
     return retorno
 
 def PegarId(match_id, game):
@@ -114,7 +136,7 @@ def PegarId(match_id, game):
                                "id: " + playerId
             index += 1
             if index == 5: details_players += "\n" + "\n" + "--------------------" + "\n"
-    return details_players
+        return details_players
 
 def PegarReplay(match_id, game):
     match_inf = game.getMatch(match_id)
@@ -151,6 +173,33 @@ def PegarStats(match_id, game):
             for dict in lista:
                 log.write('\n' + str(dict) + ' - ' + str(lista[dict]))
     log.close()
+
+def ConfereDiscId(channel_id):
+    whitelist = []
+    with open ("server_whitelist.txt", "r+") as whitelist_doc:
+        for linha in whitelist_doc.readlines():
+            nova_linha = linha.replace("\n", "")
+            whitelist.append(nova_linha)
+        whitelist_doc.close()
+    for channel in whitelist:
+        if channel == str(channel_id):
+            response = True
+            break
+        else: response = False
+    return response
+
+def RaiseChannelError(channelId, response):
+    mensagem = "Não é permitido enviar comandos nesse canal"
+    if response == False:
+        Envia_Msg(channelId, mensagem)
+        raise ValueError("Canal fora da Whitelist")
+
+def Envia_Msg(channelId, mensagem):
+    channel = client.get_channel(channelId)
+    client.loop.create_task(channel.send(mensagem))
+
+def Envia_Arquivo(channel_id, arquvio):
+    pass
 
 #Código para executar o Bot com as configurações pré-definidas
 client.run(token)
