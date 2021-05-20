ef86852cd9dd1c54e72f5a03c32864728cfd9736
antonio272a <83799815+antonio272a@users.noreply.github.com>
2021-05-20 14:19:45 -0300

    Delete pyrez directory

--- pyrez/enumerations/Region.py
+++
@@ -1,9 +0,0 @@
-from .Enum import Enum
-class Region(Enum):
-    LATIN_AMERICA_NORTH = "Latin America North"
-    BRAZIL = "Brazil"
-    EUROPE = "Europe"
-    NORTH_AMERICA = "North America"
-    SOUTHEAST_ASIA = "Southeast Asia"
-    AUSTRALIA = "Australia"
-    UNKNOWN = ''
--- pyrez/models/Smite/Team/Player.py
+++
@@ -1,8 +0,0 @@
-from pyrez.models import APIResponse
-class Player(APIResponse):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.accountLevel = kwargs.get("AccountLevel", 0) or 0
-        self.joinedDatetime = kwargs.get("JoinedDatetime", '') or ''
-        self.lastLoginDatetime = kwargs.get("LastLoginDatetime", '') or ''
-        self.name = kwargs.get("Name", '') or ''
