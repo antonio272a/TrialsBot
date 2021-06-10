a1f5aa4b30ced564dd87ef1e2c6bbb42281b391d
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-06-09 22:07:39 -0300

    optimizing API imports

--- ApiPaladinsSmite/paladinsapi.py
+++ ApiPaladinsSmite/paladinsapi.py
@@ -7,13 +7,6 @@ class PaladinsApi:
         self._dev_id_hirez = "3656"
         self._auth_key_hirez = "310114B6E36447369BBD3F35034995AC"
         self._paladins_req = pyrez.PaladinsAPI(devId=self._dev_id_hirez, authKey=self._auth_key_hirez)
-        self._call_default_requests()
-
-    def _call_default_requests(self):
-        self.champions = self._paladins_req.getChampions()
-        self.itens = self._paladins_req.getItems()
-        if not self.champions or not self.itens:
-            self._call_default_requests()
 
     def get_match_inf(self, match_id):
         return self._paladins_req.getMatch(match_id)
@@ -22,7 +15,15 @@ class PaladinsApi:
         return self._paladins_req.getPlayerId(player_name)
 
     def get_champions(self):
-        return self.champions
+        champions = self._paladins_req.getChampions()
+        if not champions:
+            self.get_champions()
+        else:
+            return champions
 
     def get_itens(self):
-        return self.itens
+        itens = self._paladins_req.getItems()
+        if not itens:
+            self.get_itens()
+        else:
+            return itens
--- ApiPaladinsSmite/smiteapi.py
+++ ApiPaladinsSmite/smiteapi.py
@@ -7,13 +7,6 @@ class SmiteApi:
         self._dev_id_hirez = "3656"
         self._auth_key_hirez = "310114B6E36447369BBD3F35034995AC"
         self._smite_req = pyrez.SmiteAPI(devId=self._dev_id_hirez, authKey=self._auth_key_hirez)
-        self._call_default_requests()
-
-    def _call_default_requests(self):
-        self.gods = self._smite_req.getGods()
-        self.itens = self._smite_req.getItems()
-        if not self.gods or not self.itens:
-            self._call_default_requests()
 
     def get_match_inf(self, match_id):
         return self._smite_req.getMatch(match_id)
@@ -22,7 +15,15 @@ class SmiteApi:
         return self._smite_req.getPlayerId(player_name)
 
     def get_gods(self):
-        return self.gods
+        gods = self._smite_req.getGods()
+        if not gods:
+            self.get_gods()
+        else:
+            return gods
 
     def get_itens(self):
-        return self.itens
+        itens = self._smite_req.getItems()
+        if not itens:
+            self.get_itens()
+        else:
+            return itens
