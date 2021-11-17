9e2e5d1ad3b99a2f237783fa9325289342cbe1f5
antonio272a <83799815+antonio272a@users.noreply.github.com>
2021-11-16 21:57:26 -0300

    Delete venv directory

--- venv/Lib/site-packages/pyrez/exceptions/PrivatePlayer.py
+++
@@ -1,4 +0,0 @@
-from .PyrezException import PyrezException
-class PrivatePlayer(PyrezException):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
--- venv/Lib/site-packages/pyrez/models/Menuitem.py
+++
@@ -1,4 +0,0 @@
-class Menuitem:
-    def __init__(self, **kwargs):
-        self.description = kwargs.get("Description", '') or ''
-        self.value = kwargs.get("Value", 0) or 0
